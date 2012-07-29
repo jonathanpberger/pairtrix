dd9d591b4bd6ef77434f590246891db7e5d7946a
jgeiger <jgeiger@gmail.com>
2012-07-29 00:05:35 -0500

    don't create new memberships if we don't have people

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -19,8 +19,9 @@ class Employee < ActiveRecord::Base
     def available
       employees = Employee.all
       employees.map do |employee|
-        employee if employee.team_memberships.empty? ||
-          employee.team_memberships.none? { |team_membership| team_membership.active? }
+        employee if (employee.team_memberships.empty? ||
+          employee.team_memberships.none? { |team_membership| team_membership.active? }) &&
+          !employee.hide?
       end.compact
     end
 
@@ -33,20 +34,22 @@ class Employee < ActiveRecord::Base
     end
   end
 
-  def solo_employee?
-    id == Employee.solo_employee.try(:id)
-  end
-
-  def out_of_office_employee?
-    id == Employee.out_of_office_employee.try(:id)
-  end
-
   def hide?
     solo_employee? || out_of_office_employee?
   end
 
   def name
     [last_name, first_name].join(", ")
+  end
+
+  private
+
+  def solo_employee?
+    id == Employee.solo_employee.try(:id)
+  end
+
+  def out_of_office_employee?
+    id == Employee.out_of_office_employee.try(:id)
   end
 
 end
