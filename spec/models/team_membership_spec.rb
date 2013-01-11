5fe673d64bd82c2346f369469a95994a0a0fb47c
jgeiger <jgeiger@gmail.com>
2013-01-10 21:42:37 -0600

    test for issue with pairs not properly deleting

--- spec/models/team_membership_spec.rb
+++ spec/models/team_membership_spec.rb
@@ -4,9 +4,7 @@ describe TeamMembership do
   describe "validations" do
     let(:team_membership) { TeamMembership.new }
 
-    before do
-      team_membership.valid?
-    end
+    before { team_membership.valid? }
 
     describe "presence" do
       it "validates the presence of team_id" do
@@ -36,6 +34,14 @@ describe TeamMembership do
           team_membership.should have(1).error_on(:employee_id)
         end
       end
+    end
+  end
+
+  describe "deleteing a team membership" do
+    it "deletes all the pair memberships and pairs associated with the employee" do
+      @pair = FactoryGirl.create(:pair_with_memberships)
+      @team_membership = @pair.team_memberships.first
+      expect { @team_membership.destroy }.to change { Pair.count }.by(-1)
     end
   end
 end
